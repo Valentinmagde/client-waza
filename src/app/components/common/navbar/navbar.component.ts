c054c514407df5f9322e4102de082823d7dbdfbb
Valentin Magde <valentinmagde@gmail.com>
2022-01-15 23:39:37 +0100

    inialisation du projet

---
+++ src/app/components/common/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+    constructor(
+        public router: Router
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+    switcherClassApplied = false;
+    switcherToggleClass() {
+        this.switcherClassApplied = !this.switcherClassApplied;
+    }
+
+    classApplied = false;
+    toggleClass() {
+        this.classApplied = !this.classApplied;
+    }
+
+}
---
+++ src/app/components/pages/become-an-instrutor-page/become-an-instrutor-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-become-an-instrutor-page',
+  templateUrl: './become-an-instrutor-page.component.html',
+  styleUrls: ['./become-an-instrutor-page.component.scss']
+})
+export class BecomeAnInstrutorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
